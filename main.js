2c1082dea104cd68e66d3541f1f70c46e1c4a1be
joaoangnes <joao.angnes@gmail.com>
2023-10-05 19:42:17 -0300

    add crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+  // Required a website argument
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+
+  // Not support more than one website
+  if (process.argv.length > 3) {
+    console.log("too many command line arguments");
+    process.exit(1);
+  }
+
+  // Get the URL
+  const baseURL = process.argv[2];
+
+  console.log("starting crawl");
+  crawlPage(baseURL);
+}
+
+main();
